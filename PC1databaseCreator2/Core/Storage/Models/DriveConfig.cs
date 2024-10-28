ae26e3b49e6bc409a91cfaf24b1dccf43a6f94d5
yjwoo591 <yjwoo@fdauto.co.kr>
2024-10-28 09:42:50 +0900

    추가변경삭제

--- PC1databaseCreator2/Core/Storage/Models/DriveConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PC1databaseCreator.Core.Storage.Models
-{
-    public class DriveConfiguration
-    {
-        public string Path { get; set; } = string.Empty;
-        public int DriveNumber { get; set; }
-        public long MinimumFreeSpace { get; set; } = 50L * 1024L * 1024L * 1024L; // 50GB
-    }
-}
